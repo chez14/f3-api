f0a5609ebbe2ef5b3ab32449dc7d5d160842eef2
Chris <christianto.g.14@gmail.com>
2018-08-31 23:58:38 +0700

    adding database auto connect

--- app/config.php
+++ app/config.php
@@ -2,3 +2,20 @@
     chdir(dirname(__DIR__));
     // Config Loader
     \F3::instance()->config("app/config/config.ini");
+
+    //connect to SQL Database.
+    \F3::set('SYSTEM.DB', false);
+    if(F3::instance()->exists('database')) {
+        try {
+            \F3::set('DB', new \DB\SQL(F3::get('database.dsn'),
+                F3::get('database.username'),
+                F3::get('database.password'), [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                ])
+            );
+            \F3::set('SYSTEM.DB', true);
+        } catch (PDOException $e){
+            \F3::set('DB', null);
+        }
+    }
